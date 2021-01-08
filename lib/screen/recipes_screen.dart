598dcc6fb3a2d3075b9629b52e1e4955cd63570a
Raj Dighe <rppdighe4892@gmail.com>
2021-01-08 22:05:41 +1100

    ADDED IOS ADAPTIVITY TO RECIEPES SCREEN.

--- lib/screen/recipes_screen.dart
+++ lib/screen/recipes_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import "dart:io";
 
 class RecipesScreen extends StatelessWidget {
   static final SCREEN = "RecipesScreen";
@@ -7,13 +9,22 @@ class RecipesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map;
     final title = routeArgs["title"];
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text("This your recipe"),
-      ),
-    );
+    return Platform.isAndroid
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+            ),
+            body: Center(
+              child: Text("This your recipe"),
+            ),
+          )
+        : CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle: Text(title),
+            ),
+            child: SafeArea(
+              child: Text("This your recipe"),
+            ),
+          );
   }
 }
